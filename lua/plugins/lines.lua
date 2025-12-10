3ebd9924dca34aa243a4b4406747b898b3417294
Tim Leupold <tim.leupold@isys.de>
2025-12-10 14:36:03 +0100

    tab name is now cwd

---
+++ lua/plugins/lines.lua
@@ -0,0 +1,9 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		tabline = {
+			lualine_a = { "string.match(vim.uv.cwd(), '([^/]+)$')" },
+		},
+	},
+}
